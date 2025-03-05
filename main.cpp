27029015bf7f9061a063b9fec5608c7fe2e284fa
Dyab <eslamdyab22@gmail.com>
2025-03-05 03:06:56 +0200

    main flow to extract ips and total bytes

--- main.cpp
+++ main.cpp
@@ -14,6 +14,8 @@ void checkNewTcpdump(PacketsMonitor *Monitor) {
 int main() {
     PacketsMonitor Monitor;
     std::string total_bytes;
+    std::unordered_map<std::string, packet>* packets_hashmap;
+    int i = 0;
 
     // Check new tcpdump data thread
     std::thread checkNewTcpdumpDataThread(checkNewTcpdump, &Monitor);
@@ -27,11 +29,21 @@ int main() {
         tcpdump_data_queue.pop();
         lock.unlock();
   
-        std::cout << traffic_captured_file_path << std::endl; 
-        total_bytes = Monitor.processNewTcpdumpTshark(traffic_captured_file_path);
-        std::cout << total_bytes << std::endl; 
-        // Monitor.processFile(traffic_captured_file_path);
+        // std::cout << traffic_captured_file_path << std::endl; 
+        total_bytes = Monitor.processNewTcpdumpTsharkTotalBytes(traffic_captured_file_path);
         
+        Monitor.processNewTcpdumpTsharkIPBytes(traffic_captured_file_path);
+        
+        
+        // std::cout << "Total Bytes IP, " << Monitor.packets_hashmap["172.29.1.167-172.29.0.1"].total_k_bytes_bandwidth_for_ip << std::endl; 
+        
+        std::cout << i << std::endl;
+        for(auto kv : Monitor.packets_hashmap) {
+            std::cout << kv.second.source_ip << ", " << kv.second.destination_ip << ", " << kv.second.total_k_bytes_bandwidth_for_ip << std::endl;
+        } 
+
+        std::cout << "----------------------------" << std::endl;
+        i++;
     }
 
 
