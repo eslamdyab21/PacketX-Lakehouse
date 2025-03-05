c871291df9fdc6cbaf552ac4bc08b3abbaa5720b
Dyab <eslamdyab22@gmail.com>
2025-03-05 07:06:25 +0200

    main flow and prints total bandwidth usage

--- main.cpp
+++ main.cpp
@@ -15,35 +15,31 @@ int main() {
     PacketsMonitor Monitor;
     std::string total_bytes;
     std::unordered_map<std::string, packet>* packets_hashmap;
-    int i = 0;
 
     // Check new tcpdump data thread
     std::thread checkNewTcpdumpDataThread(checkNewTcpdump, &Monitor);
 
     // Process new tcpdump data main thread
     while (true) {
-        std::unique_lock<std::mutex> lock(queue_mutex);
-        queue_cond.wait(lock, [] { return !tcpdump_data_queue.empty(); });
+        std::unique_lock<std::mutex> lock(Monitor.queue_mutex);
+        // Monitor.queue_cond.wait(lock, [&] { return !Monitor.tcpdump_data_queue.empty(); });
+        Monitor.queue_cond.wait(lock);
 
-        std::string traffic_captured_file_path = tcpdump_data_queue.front();
-        tcpdump_data_queue.pop();
+        std::string traffic_captured_file_path = Monitor.tcpdump_data_queue.front();
+        Monitor.tcpdump_data_queue.pop();
         lock.unlock();
   
-        // std::cout << traffic_captured_file_path << std::endl; 
-        total_bytes = Monitor.processNewTcpdumpTsharkTotalBytes(traffic_captured_file_path);
+        Monitor.processNewTcpdumpTsharkTotalBytes(traffic_captured_file_path);
         
         Monitor.processNewTcpdumpTsharkIPBytes(traffic_captured_file_path);
         
-        
-        // std::cout << "Total Bytes IP, " << Monitor.packets_hashmap["172.29.1.167-172.29.0.1"].total_k_bytes_bandwidth_for_ip << std::endl; 
-        
-        std::cout << i << std::endl;
+                
+        std::cout << traffic_captured_file_path << std::endl;
         for(auto kv : Monitor.packets_hashmap) {
-            std::cout << kv.second.source_ip << ", " << kv.second.destination_ip << ", " << kv.second.total_k_bytes_bandwidth_for_ip << std::endl;
+            std::cout << kv.second.source_ip << ", " << kv.second.destination_ip << ", " << kv.second.total_k_bytes_bandwidth_for_ip << ", " << Monitor.total_bytes_all_ips << std::endl;
         } 
 
         std::cout << "----------------------------" << std::endl;
-        i++;
     }
 
 
--- tcpdump_capture.sh
+++ tcpdump_capture.sh
@@ -7,7 +7,8 @@ TIME=10
 mkdir -p .tcpdump
 chmod -R 777 .tcpdump
 echo "📡 Starting Packet Core Monitoring..."
+
+
 while true; do
     tcpdump -i $INTERFACE -s 0 -B 4096 -w .tcpdump/capture-%Y%m%d%H%M%S.pcap -G "$TIME" -v
-    touch .tcpdump/tcpdump_finished_writing
 done
