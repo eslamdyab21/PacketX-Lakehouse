0fa0da5b12dd381c3b474e1d297c655b87d8d84e
Dyab <eslamdyab22@gmail.com>
2025-04-08 21:32:29 +0200

    first date dimenstion etl

--- warehouse_etl.py
+++ warehouse_etl.py
@@ -1,7 +1,7 @@
 from pyiceberg.catalog import load_catalog
 from dotenv import load_dotenv
 import pyarrow.compute as pc
-import pyarrow as pa
+from pathlib import Path
 import configparser
 from utils import *
 import logging
@@ -9,11 +9,21 @@ import duckdb
 import os
 
 
+def date_dim_etl(con, sql_base_path, filter_date):
+    logging.info(f"""date_dim_etl""")
+
+    sql_query = Path(sql_base_path + '/date_dim_etl.sql').read_text()
+    con.execute(sql_query, [filter_date])
+
+    logging.info(f"""date_dim_etl -> Done""")
+
+
+
 def aggregate_by_hour(table):
     logging.info(f"""aggregate_by_hour""")
 
     # Truncate timestamp to hour
-    time_hour = pc.strftime(table["time_stamp"], format="%Y-%m-%d %H:00")
+    time_hour = pc.floor_temporal(table["time_stamp"], unit="hour")
     table = table.append_column("time_hour", time_hour)
 
 
@@ -60,6 +70,7 @@ if __name__ == "__main__":
     config.read_file(open(r'conf'))
     local_or_aws       = config.get('Warehouse ETL', 'local_or_aws')
     filter_date        = config.get('Warehouse ETL', 'filter_date')
+    sql_base_path      = config.get('Warehouse ETL', 'sql_base_path')
     logging.info(f"""Main -> Load Conf and Envs -> Done""")
     # ----- Load .env and conf -----
 
@@ -79,4 +90,7 @@ if __name__ == "__main__":
         filtered_day_table = aggregate_by_hour(filtered_day_table.to_arrow())
 
         con.register("lakehouse_packets", filtered_day_table)
+
+        date_dim_etl(con, sql_base_path, filter_date)
+        con.close()
         # ----- SQL Lite Local Path -----
