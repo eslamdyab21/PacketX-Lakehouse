14933f4d5b0a836ce70095df94b4dbb01907b3b4
Dyab <eslamdyab22@gmail.com>
2025-04-08 18:38:09 +0200

    connect to postgres and pyarrow tables with duckdb

--- warehouse_etl.py
+++ warehouse_etl.py
@@ -1,10 +1,51 @@
 from pyiceberg.catalog import load_catalog
 from dotenv import load_dotenv
+import pyarrow.compute as pc
+import pyarrow as pa
 import configparser
 from utils import *
 import logging
+import duckdb
 import os
 
+
+def aggregate_by_hour(table):
+    logging.info(f"""aggregate_by_hour""")
+
+    # Truncate timestamp to hour
+    time_hour = pc.strftime(table["time_stamp"], format="%Y-%m-%d %H:00")
+    table = table.append_column("time_hour", time_hour)
+
+
+    # Group by time_hour, source_ip, destination_ip and sum bandwidth_kb
+    result = table.group_by(keys=["time_hour", "user","source_ip", "destination_ip"]).\
+                            aggregate([("bandwidth_kb", "sum")])
+
+
+    logging.info(f"""aggregate_by_hour -> Done""")
+    return result
+
+
+
+def connect_to_postgres_wh():
+    logging.info(f"""connect_to_postgres_wh""")
+
+    con = duckdb.connect()  
+    con.sql(f"""
+        INSTALL postgres;
+        LOAD postgres;  
+        ATTACH '  
+            dbname={os.getenv("POSTGRES_NAME")}
+            hostaddr={os.getenv("POSTGRES_HOST")}  
+            port={os.getenv("POSTGRES_PORT")}  
+            user={os.getenv("POSTGRES_USER")}  
+            password={os.getenv("POSTGRES_PASSWORD")}  
+        ' AS db (TYPE postgres);  
+    """)
+
+
+    logging.info(f"""connect_to_postgres_wh -> Done""")
+    return con
 
 
 
@@ -17,9 +58,8 @@ if __name__ == "__main__":
     load_dotenv()
     config = configparser.ConfigParser()
     config.read_file(open(r'conf'))
-    s3_lakehouse_path  = config.get('Raw S3 Iceberg Lakehouse ETL', 's3_lakehouse_path')
-    region_name        = config.get('Raw S3 Iceberg Lakehouse ETL', 'region_name')
     local_or_aws       = config.get('Warehouse ETL', 'local_or_aws')
+    filter_date        = config.get('Warehouse ETL', 'filter_date')
     logging.info(f"""Main -> Load Conf and Envs -> Done""")
     # ----- Load .env and conf -----
 
@@ -31,5 +71,12 @@ if __name__ == "__main__":
         # ----- SQL Lite Local Path -----
         catalog = load_local_sqlite_catalog()
         iceberg_table = catalog.load_table("PacketX_Raw.Packets")
-        
+        con = connect_to_postgres_wh()
+
+        start_time = f"{filter_date}T00:00:00"
+        end_time   = f"{filter_date}T23:59:59"
+        filtered_day_table = iceberg_table.scan(row_filter=f"time_stamp >= '{start_time}' AND time_stamp <= '{end_time}'")
+        filtered_day_table = aggregate_by_hour(filtered_day_table.to_arrow())
+
+        con.register("lakehouse_packets", filtered_day_table)
         # ----- SQL Lite Local Path -----
