d4444d49b4957d124f4162efdcf5bb4e8f30b10d
Dyab <eslamdyab22@gmail.com>
2025-04-10 01:33:45 +0200

    alter direction dimension unique

--- warehouse-migrations/postgress/sql/V1.04__create_direction_dim.sql
+++ warehouse-migrations/postgress/sql/V1.04__create_direction_dim.sql
@@ -1,6 +1,6 @@
 CREATE TABLE direction_dim (
     direction_key          SERIAL PRIMARY KEY,
-    direction              VARCHAR(8),
+    direction              VARCHAR(8) UNIQUE,
 
     CHECK (direction IN ('inbound', 'outbound'))
 );
