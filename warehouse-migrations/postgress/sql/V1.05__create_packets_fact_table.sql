4ffbb4696188cd933e76178a996b0ef69f0279d2
Dyab <eslamdyab22@gmail.com>
2025-04-10 16:14:05 +0200

    add a new external_ip column

--- warehouse-migrations/postgress/sql/V1.05__create_packets_fact_table.sql
+++ warehouse-migrations/postgress/sql/V1.05__create_packets_fact_table.sql
@@ -2,8 +2,9 @@ CREATE TABLE packets_fact (
     packets_key          SERIAL PRIMARY KEY,
     user_key             INT REFERENCES users_dim(user_key),
     source_ip_key        INT REFERENCES ip_dim(ip_key),
-    dest_ip_key          INT REFERENCES ip_dim(ip_key),
+    destination_ip_key   INT REFERENCES ip_dim(ip_key),
+    external_ip_key      INT REFERENCES ip_dim(ip_key),
     date_key             INT REFERENCES date_dim(date_key),
     direction_key        INT REFERENCES direction_dim(direction_key),
     kb_bandwidth         FLOAT
-);
+)
